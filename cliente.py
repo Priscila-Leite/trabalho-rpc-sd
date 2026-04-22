996a92c04d5f51b70b63bd12e9aad080c68e3862
Dawson Monteiro <dwsoliv73@users.noreply.github.com>
2026-04-22 15:47:24 -0300

    feat: adicionar servidor e cliente básico (Q1-Q2)

---
+++ cliente.py
@@ -0,0 +1,16 @@
+import rpyc
+import sys
+import time
+
+
+if len(sys.argv) < 2:
+    exit("Usage: {} SERVER".format(sys.argv[0]))
+
+server = sys.argv[1]
+
+conn = rpyc.connect(server, 18861)
+
+# --- Questões 1 e 2: Testes básicos ---
+print(conn.root)
+print(conn.root.get_answer())
+print(conn.root.the_real_answer_though)
---
+++ servidor.py
@@ -0,0 +1,36 @@
+import rpyc
+import time
+
+
+class MyService(rpyc.Service):
+    def on_connect(self, conn):
+        # código executado quando uma conexão é iniciada
+        print("Cliente conectado!")
+
+    def on_disconnect(self, conn):
+        # código executado quando uma conexão é finalizada
+        print("Cliente desconectado!")
+
+    def exposed_get_answer(self):  # método exposto
+        return 42
+
+    exposed_the_real_answer_though = 43  # atributo exposto
+
+    def get_question(self):  # método NÃO exposto
+        return "Qual é a cor do cavalo branco de Napoleão?"
+
+    def exposed_sum_vector(self, vector):
+        """Recebe um vetor e retorna a soma dos elementos."""
+        start = time.time()
+        resultado = sum(vector)
+        end = time.time()
+        print(f"Tempo de execução no servidor: {end - start}")
+        return resultado
+
+
+# Para iniciar o servidor
+if __name__ == "__main__":
+    from rpyc.utils.server import ThreadedServer
+    t = ThreadedServer(MyService, port=18861)
+    print("Servidor iniciado na porta 18861...")
+    t.start()
